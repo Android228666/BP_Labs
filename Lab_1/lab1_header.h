295f6ac900d1d8670b8d30d99a4629852c2e7fc5
Andriy <63637126+Android228666@users.noreply.github.com>
2023-04-10 09:58:03 +0100

    Create lab1_header.h

---
+++ Lab_1/lab1_header.h
@@ -0,0 +1,25 @@
+using namespace std;
+
+//<--------------------- C++ --------------------->
+void cpp_write_to_file(string);
+
+void cpp_read_file(string);
+
+void cpp_delete_duplicates(string, string);
+
+void cpp_functions_caller(string, string);
+
+void cpp_append_data(string, string);
+
+
+
+//<--------------------- C --------------------->
+void write_to_file(char*);
+
+void read_file(char*);
+
+void delete_duplicates(char*, char*);
+
+void c_functions_caller(char*, char*);
+
+void append_data(char*, char*);
